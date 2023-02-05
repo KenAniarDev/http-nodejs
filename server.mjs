6447ae21197ddcad00cb1c6acb6999b1defec9a3
Nestor Kenneth Aniar <ken.aniar.dev@gmail.com>
2023-02-05 17:58:24 +0800

    change output

--- server.mjs
+++ server.mjs
@@ -1,7 +1,8 @@
-import { createServer } from 'http';
-import dayjs from 'dayjs';
-
+import { createServer } from "http";
+import dayjs from "dayjs";
+const PORT = process.env.PORT || "8080";
+console.log(PORT);
 createServer((req, res) => {
   res.write(dayjs(new Date()).format("DD/MM/YYYY hh:mm:ss"));
   res.end();
-}).listen(process.env.PORT);
+}).listen(PORT);
