efa15b3870790e3892dd56484ce36e918f1e8f2b
Nestor Kenneth Tesorio Aniar <68641803+KenAniarDev@users.noreply.github.com>
2023-02-05 17:55:15 +0800

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import { createServer } from 'http';
+import dayjs from 'dayjs';
 
 createServer((req, res) => {
-  res.write('Hello World!');
+  res.write(dayjs(new Date()).format("DD/MM/YYYY hh:mm:ss"));
   res.end();
 }).listen(process.env.PORT);
